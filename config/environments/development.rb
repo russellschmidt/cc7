c94e4262d08ae14135e7209305cea330e08c9131
Russell Schmidt <russell.schmidt@me.com>
2017-02-07 20:40:43 -0800

    add devise models Donor and Admin, config route, add req'd line to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # set up default url options for action_mailer per Devise gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

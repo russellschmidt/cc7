444eb2f40db86b96af1b21841554d6cf94770aaa
Russell Schmidt <russell.schmidt@me.com>
2017-02-10 20:05:46 -0800

    add seeds for partners, corrected views

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,15 @@ Partner.delete_all
 partner = Partner.new(name: "The Bay Foundation", slug:"the_bay_foundation",
   logo_aws_url: "https://s3.amazonaws.com/climatecents/logos/Bay-Foundation-logo-155x155.png")
 partner.save
+
+partner = Partner.new(name: "MEND - Meet Each Need with Dignity", slug: "MEND_meet_each_need_with_dignity",
+  logo_aws_url: "https://s3.amazonaws.com/climatecents/logos/MEND-logo.jpg")
+partner.save
+
+partner = Partner.new(name: "Isidore Electronics Recycling", slug: "isidore_electronics_recycling",
+  logo_aws_url: "https://s3.amazonaws.com/climatecents/logos/Isidore-Logo-155.png")
+partner.save
+
+partner = Partner.new(name: "GRID Alternatives", slug: "GRID_alternatives",
+  logo_aws_url: "https://s3.amazonaws.com/climatecents/logos/GridAlternativeslogo.png")
+partner.save

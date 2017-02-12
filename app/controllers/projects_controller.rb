91e2c06ba412d4e334c812110bd8a85cdf072b20
Russell Schmidt <russell.schmidt@me.com>
2017-02-11 16:40:01 -0800

    add project#update action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,16 +17,27 @@ class ProjectsController < ApplicationController
   def create
     @project = @location.projects.build(project_params)
     if @project.save
-      redirect_to location_projects_path, notice: "Successfully created project."
+      flash[:notice] = "Successfully created project."
+      redirect_to location_projects_path
     else
-      render action: "new"
+      flash[:notice] = "Create error, please try again."
+      render :new
     end
   end
 
   def edit
+    @project = @location.projects.friendly.find(params[:id])
   end
 
   def update
+    @project = @location.projects.friendly.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:notice] = 'Updated partner successfully'
+      redirect_to location_projects_path
+    else
+      flash[:notice] = 'Save error, please try again'
+      render :edit
+    end
   end
 
   def destroy
